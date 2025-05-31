00acef4384189c031df91e4d99fcb64550720583
Buffett <bliu@quid.com>
2025-05-31 14:48:52 +0800

    File path change

--- src/ddw_chain.sh
+++ src/ddw_chain.sh
@@ -1,5 +1,5 @@
 python ddw_render_chain.py \
-    --plugin /Library/Audio/Plug-Ins/VST/Serum.vst \
+    --plugin /Users/bliu/Library/Audio/Plug-Ins/VST/Serum.vst \
     --preset-dir ../fxp_preset/lead \
     --sample-rate 44100 \
     --bpm 120 \
